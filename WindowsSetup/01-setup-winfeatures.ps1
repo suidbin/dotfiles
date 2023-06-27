b595c844a656b1ae75af671b35c1a6631002241e
suidbin <s2pft8kns8@liamekaens.com>
2023-06-27 14:21:33 -0400

    vagrant setup, triggers, and ssh_key management (#2)
    * custom Vagrant triggers and ssh_key writing
    
    * windows vagrant configuration and setup

--- WindowsSetup/01-setup-winfeatures.ps1
+++ WindowsSetup/01-setup-winfeatures.ps1
@@ -2,7 +2,11 @@
 
 Write-Verbose "Enabling Windows Defender Application Guard..."
 Enable-WindowsOptionalFeature -Online -NoRestart -All -FeatureName Windows-Defender-ApplicationGuard
-# Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+Write-Verbose "Enabling Windows Hyper-V..."
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+Add-LocalGroupMember -Group "Hyper-V Administrators" -Member $env:username
+
 # Sandbox: Containers-DisposableClientVM
 # Microsoft-Hyper-V-All
 # HypervisorPlatform
